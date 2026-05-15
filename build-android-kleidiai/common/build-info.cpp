88293c1dc82aa9a37ac3bdb5eb0ad118fad6c26c
Siddhesh Sonar <darkwildhacker@gmail.com>
2026-05-16 00:11:51 +0530

    build: refresh android-kleidiai artifacts

--- build-android-kleidiai/common/build-info.cpp
+++ build-android-kleidiai/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 4;
-char const *LLAMA_COMMIT = "e92abb304";
+int LLAMA_BUILD_NUMBER = 16;
+char const *LLAMA_COMMIT = "87265e3a5";
 char const *LLAMA_COMPILER = "Clang 19.0.1";
 char const *LLAMA_BUILD_TARGET = "Android aarch64";
